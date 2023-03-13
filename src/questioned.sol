d8b208bc0230f9ea02c193127428e8d495a55405
Aryan Sangwan <55026638+ATV3320@users.noreply.github.com>
2023-03-13 23:40:44 +0530

    blacnk

--- src/questioned.sol
+++ src/questioned.sol
@@ -1,7 +1,7 @@
-import { HuffDeployer } from "foundry-huff/HuffDeployer.sol";
+// import { HuffDeployer } from "foundry-huff/HuffDeployer.sol";
 
-contract HuffDeploymentExample {
-    function deploy() external returns(address) {
-        return new HuffDeployer().deploy("MyContract");
-    }
-}
+// contract HuffDeploymentExample {
+//     function deploy() external returns(address) {
+//         return new HuffDeployer().deploy("MyContract");
+//     }
+// }
